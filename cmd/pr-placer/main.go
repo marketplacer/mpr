2f35884f7f4ed8ff75165b3b9b615bb6f72e4658
Ellis Clayton <ellis@ellis.codes>
2020-08-20 20:02:34 +1000

    feat: Add more options

--- cmd/pr-placer/main.go
+++ cmd/pr-placer/main.go
@@ -1,31 +1,136 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/manifoldco/promptui"
+	"github.com/tcnksm/go-input"
+	"log"
+	"os"
 )
 
-type work struct {
-	Description string
-	Key         string
+type option struct {
+	key   string
+	value string
 }
 
-func main() {
+func selectFromOptions(question string, options []option) string {
+	optionValues := make([]string, len(options))
+	for i, opt := range options {
+		optionValues[i] = opt.value
+	}
+
 	prompt := promptui.Select{
-		Label: "Who can notice the change?",
-		Items: []string{
-			"Users (bugs, features, performance)",
-			"Just us (docs, ci, code style)",
-		},
+		Label: question,
+		Items: optionValues,
 	}
 
 	i, _, err := prompt.Run()
 
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
+		log.Fatal(err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("You choose %d\n", i)
+	return options[i].key
+}
+
+func getChangeType() string {
+	changeTypes := []option{
+		option{
+			key:   "external",
+			value: "Users (bugs, features, performance)",
+		},
+		option{
+			key:   "internal",
+			value: "Just us (docs, ci, code style)",
+		},
+	}
+
+	return selectFromOptions("Who can notice the change?", changeTypes)
+}
+
+func getExternalChangeType() string {
+	changeTypes := []option{
+		option{
+			key:   "feat",
+			value: "A new feature",
+		},
+		option{
+			key:   "fix",
+			value: "A bug fix",
+		},
+		option{
+			key:   "perf",
+			value: "A performance improvement",
+		},
+		option{
+			key:   "docs",
+			value: "External, documentation only changes (e.g. API docs)",
+		},
+	}
+
+	return selectFromOptions("What sort of change is it?", changeTypes)
+}
+
+func getInternalChangeType() string {
+	changeTypes := []option{
+		option{
+			key:   "ci",
+			value: "Changes purely to our CI configuration files and scripts",
+		},
+		option{
+			key:   "build",
+			value: "Changes that affect how we compile or execute code",
+		},
+		option{
+			key:   "docs",
+			value: "Documentation only changes",
+		},
+		option{
+			key:   "refactor",
+			value: "Refactoring code without changing what it does",
+		},
+		option{
+			key:   "style",
+			value: "Code style changes (e.g. new linter rule)",
+		},
+		option{
+			key:   "test",
+			value: "Adding missing tests or correcting existing tests",
+		},
+	}
+
+	return selectFromOptions("What sort of change is it?", changeTypes)
+}
+
+func getTitle() string {
+	ui := &input.UI{}
+	query := "What's the one-sentence title of the change?"
+	name, err := ui.Ask(query, &input.Options{
+		HideOrder: true,
+		Loop:      true,
+		Required:  true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	return name
+}
+
+func main() {
+	changeType := getChangeType()
+
+	var conventionalType string
+
+	if changeType == "external" {
+		conventionalType = getExternalChangeType()
+	} else {
+		conventionalType = getInternalChangeType()
+	}
+
+	log.Printf("%s\n", conventionalType)
+
+	getTitle()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/marketplacer/pr-placer
 
 go 1.14
 
-require github.com/manifoldco/promptui v0.7.0
+require (
+	github.com/manifoldco/promptui v0.7.0
+	github.com/tcnksm/go-input v0.0.0-20180404061846-548a7d7a8ee8
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+)
